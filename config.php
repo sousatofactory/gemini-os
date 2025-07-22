17b84e1b6c2e38a054c67dbae96e2b6dd6ee22ac
sousatofactory <kurtakashi@hotmail.com>
2025-07-22 12:48:33 -0300

    "Rebuild-config-confirmed"

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
    2 // config.php
    3 // Este arquivo deve ser colocado FORA da pasta public_html (ou www) do seu site.
    4 // Exemplo: se seu site está em /home/usuario/public_html, coloque este arquivo em /home/usuario/
-   5 define('GEMINI_API_KEY', 'AIzaSyBNBMrgYujPUOkrzYP1--jJSADKSm_yOc8');
+   5 define('GEMINI_API_KEY', 'AIzaSyAoGn3BMoBaTtFfqAxmE5KbFR9o5NfXBS0');
    6 ?>
